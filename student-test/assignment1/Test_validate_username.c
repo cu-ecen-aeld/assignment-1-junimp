96fee0e6a5fceab88fe6319217cd1d4a04fb803a
junimp <junaidimpex@gmail.com>
2023-12-25 17:47:55 +0500

    assignemt-1 done by junimp

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,12 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *my_name = my_username();
+    
+    char * buff = malloc_username_from_conf_file() ;
+    
+  TEST_ASSERT_EQUAL_STRING_MESSAGE(my_name, buff, "not equal.This will be printed when the strings are unequal.");
+    
+  free(buff);
+
 }
